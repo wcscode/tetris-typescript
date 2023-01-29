38fcd5f81b24885685128d3a463081ddf2673173
William Cristiano da Silva <wileao@gmail.com>
2023-01-28 23:00:27 -0300

    Initial commit

--- build/StartScene.js
+++
@@ -1,8 +0,0 @@
-import Scene from "./Engine/Scene.js";
-import StartMenu from "./StartMenu.js";
-export default class StartScene extends Scene {
-    build() {
-        const startMenu = new StartMenu();
-        this._gamesObjects.push(startMenu);
-    }
-}
--- src/Engine/ILoop.ts
+++
@@ -1,6 +0,0 @@
-export default interface ILoop
-{
-    build(): void;
-    update(deltaTime: number): void;
-    render(): void;
-}
