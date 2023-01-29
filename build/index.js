2720fbe670d0e890c4ed9af765424efdae5acc61
William Cristiano da Silva <wileao@gmail.com>
2023-01-28 23:55:02 -0300

    initial setup

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const container = document.getElementById('container');
+if (container !== null)
+    container.innerHTML = 'Loaded!!!';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const container: HTMLElement | null = document.getElementById('container');
+
+if(container !== null)
+    container.innerHTML = 'Loaded!!!';
